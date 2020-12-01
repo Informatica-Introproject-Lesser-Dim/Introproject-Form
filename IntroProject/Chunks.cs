21727bf05c6f8e4537ceb352f4cb93f4e4c1a14e
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-12-01 19:46:29 +0100

    Hotfix last merge commit

--- IntroProject/Chunks.cs
+++ IntroProject/Chunks.cs
@@ -4,7 +4,7 @@ namespace IntroProject
 {
     public class Chunks
     {
-        Kaart kaart;
+        Map kaart;
         List<Entity> entities;
         //Een map subsectie, met een regio aanduiding door middel van een kaart classe en een Entity list voor entities in de kaart regio.
     }
