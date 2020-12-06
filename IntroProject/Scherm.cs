389d9a1727044c91a2eb57ce5206fccec78c1398
itsMacfarlane <fca.muntz@gmail.com>
2020-12-06 18:34:32 +0100

    Start Button Code

--- IntroProject/Scherm.cs
+++ IntroProject/Scherm.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Drawing;
 
 namespace IntroProject
 {
@@ -16,6 +12,30 @@ namespace IntroProject
         {
             InitializeComponent();
 
+            Button play = new Button();
+            Button stop = new Button();
+            Button pause = new Button();
+            Button settings = new Button();
+            Button statistics = new Button();
+            Button exit = new Button();
+            Button help = new Button();
+            play.Image = IntroProject.Properties.Resources.Play_icon;
+            stop.Image = IntroProject.Properties.Resources.Stop_icon;
+            pause.Image = IntroProject.Properties.Resources.Pause_icon;
+            settings.Image = IntroProject.Properties.Resources.Settings_icon;
+            statistics.Image = IntroProject.Properties.Resources.Graph_icon;
+            exit.Image = IntroProject.Properties.Resources.X_icon;
+            help.Image = IntroProject.Properties.Resources.Help_icon;
+            
+            pause.Location = new Point (40, 40);
+            play.Location = new Point(80, 40);
+            stop.Location = new Point(120, 40);
+
+            pause.AutoSize = true;
+
+            this.Controls.Add(pause);
+            this.Controls.Add(play);
+            this.Controls.Add(stop);
             // (mogelijk) na informatie van de user is verkregen maak een processor classe, om het ecosysteem op te zetten.
         }
 
