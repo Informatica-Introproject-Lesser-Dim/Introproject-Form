0cb2883adc76e7493fc3d46a310d62d32a4d5b47
Vincent Hoffmann <v.a.hoffmann@students.uu.nl>
2020-12-06 17:38:19 +0100

    Added Buttons Examples

--- IntroProject/Scherm.cs
+++ IntroProject/Scherm.cs
@@ -16,6 +16,15 @@ namespace IntroProject
         {
             InitializeComponent();
 
+            //HIER ADDE IK DE BUTTONS
+            Button play = new Button();
+            Button stop = new Button();
+            Button pause = new Button();
+            Button settings = new Button();
+            Button statistics = new Button();
+            // play.Image = this.BackgroundImage path
+            // settings.Location = (Point) this.Width - 200, this.Height - 500;
+
             // (mogelijk) na informatie van de user is verkregen maak een processor classe, om het ecosysteem op te zetten.
         }
 
