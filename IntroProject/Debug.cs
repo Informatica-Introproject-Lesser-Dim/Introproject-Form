75d2c2775f208288d8fab2a0785c92e9581bc187
Vincent Hoffmann <v.a.hoffmann@students.uu.nl>
2020-12-07 13:53:40 +0100

    Correcte Button Locatie

--- IntroProject/Debug.cs
+++ IntroProject/Debug.cs
@@ -32,8 +32,53 @@ namespace IntroProject
         int[] pos = new int[2] { 0, 0 };
         Font font = new Font("Arial", 12);
 
-        public DebugScreen(int w, int h) 
+        public DebugScreen(int w, int h)
         {
+            ToolStrip strip = new ToolStrip();
+            Button play = new Button();
+            Button stop = new Button();
+            Button pause = new Button();
+            Button settings = new Button();
+            Button statistics = new Button();
+            Button exit = new Button();
+            Button help = new Button();
+            strip.Dock = DockStyle.Right;
+            ToolStripTextBox box = new ToolStripTextBox();
+
+            strip.Items.Add(box);
+
+            play.BackgroundImage = Properties.Resources.Play_icon;
+            play.BackgroundImageLayout = ImageLayout.Stretch;
+            stop.BackgroundImage = Properties.Resources.Stop_icon;
+            stop.BackgroundImageLayout = ImageLayout.Stretch;
+            pause.BackgroundImage = Properties.Resources.Pause_icon;
+            pause.BackgroundImageLayout = ImageLayout.Stretch;
+            settings.BackgroundImage = Properties.Resources.Settings_icon;
+            settings.BackgroundImageLayout = ImageLayout.Stretch;
+            statistics.BackgroundImage = Properties.Resources.Graph_icon;
+            statistics.BackgroundImageLayout = ImageLayout.Stretch;
+            exit.BackgroundImage = Properties.Resources.X_icon;
+            exit.BackgroundImageLayout = ImageLayout.Stretch;
+            help.BackgroundImage = Properties.Resources.Help_icon;
+            help.BackgroundImageLayout = ImageLayout.Stretch;
+
+
+            pause.Size = new Size(40, 40);
+            play.Size = new Size(40, 40);
+            stop.Size = new Size(40, 40);
+
+
+            pause.Location = new Point(40, 40);
+            play.Location = new Point(80, 40);
+            stop.Location = new Point(120, 40);
+
+            pause.AutoSize = true;
+
+
+            this.Controls.Add(pause);
+            this.Controls.Add(play);
+            this.Controls.Add(stop);
+
             this.Size = new Size(w, h);
             this.Paint += drawScreen;
             kaart = new Map(200, 150, 5, 0);
--- IntroProject/Scherm.cs
+++ IntroProject/Scherm.cs
@@ -12,30 +12,6 @@ namespace IntroProject
         {
             InitializeComponent();
 
-            Button play = new Button();
-            Button stop = new Button();
-            Button pause = new Button();
-            Button settings = new Button();
-            Button statistics = new Button();
-            Button exit = new Button();
-            Button help = new Button();
-            play.Image = IntroProject.Properties.Resources.Play_icon;
-            stop.Image = IntroProject.Properties.Resources.Stop_icon;
-            pause.Image = IntroProject.Properties.Resources.Pause_icon;
-            settings.Image = IntroProject.Properties.Resources.Settings_icon;
-            statistics.Image = IntroProject.Properties.Resources.Graph_icon;
-            exit.Image = IntroProject.Properties.Resources.X_icon;
-            help.Image = IntroProject.Properties.Resources.Help_icon;
-            
-            pause.Location = new Point (40, 40);
-            play.Location = new Point(80, 40);
-            stop.Location = new Point(120, 40);
-
-            pause.AutoSize = true;
-
-            this.Controls.Add(pause);
-            this.Controls.Add(play);
-            this.Controls.Add(stop);
             // (mogelijk) na informatie van de user is verkregen maak een processor classe, om het ecosysteem op te zetten.
         }
 
@@ -44,4 +20,5 @@ namespace IntroProject
 
         }
     }
+
 }
