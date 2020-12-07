4190e1b0334fb450f31ad03824f06f351aa31c85
Joost van der Laan <j.vanderlaan1@students.uu.nl>
2020-12-07 18:21:26 +0100

    added a base for route creating

---
+++ IntroProject/AStar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroProject
+{
+    class AStar
+    { //this is where every entity saves it's jumpheight and speed etc you 
+        
+    }
+}
---
+++ IntroProject/Route.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroProject
+{
+    class Route
+    {
+        List<int> points;
+        Point start;
+        Point end;
+        List<int> distances;
+        int pos;
+        int Length;
+        int size;
+
+        public Route(Point start, int size) {
+            this.start = start;
+            pos = 0;
+            Length = 0;
+        }
+
+        public void addDirection(int n) {
+            if (points == null) {
+                points.Add(n);
+                //find the correct edge position and calc the distance towards it   
+                //return
+            }
+            //add one of the points normally and look up the distance
+        }
+
+    }
+}
