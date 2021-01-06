b44939f5472906e6af37bc42c5e0d64d33bd1631
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-06 16:51:40 +0100

    Add MultiLanguageTest: TestHeaderWithTwoTranslations

--- IntroProjectTest/Screen.cs
+++ IntroProjectTest/Screen.cs
@@ -40,6 +40,50 @@ namespace IntroProjectTest
                         }
                     }
                 }
+
+                [TestFixture]
+                public class TestHeaderWithTwoTranslations
+                {
+                    MultiLanguage multiLanguage = new MultiLanguage(@".\fixtures\translations\header-with-two-translations.csv");
+                    private const int languageCount = 2;
+                    private const int translationEntriesCount = 2;
+
+                    Dictionary<string, string>[] dicts;
+                    string[] langs;
+
+                    [SetUp]
+                    public void SetUp()
+                    {
+                        dicts = new Dictionary<string, string>[languageCount];
+                        multiLanguage.translations.Values.CopyTo(dicts, 0);
+                        langs = new string[languageCount];
+                        multiLanguage.translations.Keys.CopyTo(langs, 0);
+                    }
+
+                    [Test]
+                    public void TestTranslationDictionaryValuesFilledWithTranslations()
+                    {
+                        Assert.AreEqual(languageCount, multiLanguage.translations.Count);
+                        foreach(var dict in dicts)
+                            Assert.AreEqual(translationEntriesCount, dict.Count);
+
+                        for(int i = 0; i < dicts.Length; i++)
+                            switch (langs[i])
+                            {
+                                case "Dutch":
+                                    Assert.AreEqual(dicts[i]["banana"], "Banaan");
+                                    Assert.AreEqual(dicts[i]["hp"], "Aantal levenspunten");
+                                    break;
+                                case "English":
+                                    Assert.AreEqual(dicts[i]["banana"], "Banana");
+                                    Assert.AreEqual(dicts[i]["hp"], "Amount of healthpoints");
+                                    break;
+                                default:
+                                    Assert.Fail("Unknown language");
+                                    break;
+                            }
+                    }
+                }
             }
         }
     }
