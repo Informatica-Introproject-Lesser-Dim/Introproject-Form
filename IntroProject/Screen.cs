2d0789688be5777960a0502b64e17fe62b339013
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-06 19:21:31 +0100

    Move MultipleLanguages o.f. Screen.cs, into Core/Utils/

--- IntroProject/Screen.cs
+++ IntroProject/Screen.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
-using System.Collections.Generic;
 
 namespace IntroProject
 {
-    using Language = String;
     public partial class Screen : Form
     {
         MapScreen debugscr;
@@ -453,62 +450,6 @@ namespace IntroProject
             this.Controls.Add(exit);
         }
     }
-    public class MultiLanguage
-    {
-        bool debugdisplayedText = true;
-
-        public Dictionary<Language, Dictionary<string, string>> translations = new Dictionary<Language, Dictionary<string, string>>();
-
-        private StreamReader reader;
-        private string[] headerSplit;
-
-        public MultiLanguage() : this(@".\dataFiles\translations.csv") { }
-
-        public MultiLanguage(string translationFile)
-        {
-            reader = new StreamReader(translationFile);
-            ReadLanguagesFromTranslationsFileHeader();
-            ReadTranslationsFromTranslationsFile();
-        }
-
-        private void ReadLanguagesFromTranslationsFileHeader()
-        {
-            if (reader.EndOfStream)
-                throw new FileLoadException("Missing header; End of Filestream reached");
-            headerSplit = reader.ReadLine().Split(',');
-
-            for (int i = 1; i < headerSplit.Length; i++)
-                translations.Add(headerSplit[i], new Dictionary<string, string>());
-        }
-
-        private void ReadTranslationsFromTranslationsFile()
-        {
-            for (int i = 0; !reader.EndOfStream; i++)
-            {
-                string[] entries = reader.ReadLine().Split(',');
-                string key = entries[0];
-                for (int j = 1; j < entries.Length; j++)
-                {
-                    string translated = entries[j];
-                    // Add (key => translated) per language
-                    translations[headerSplit[j]].Add(key, translated);
-                }
-            }
-            reader.Close();
-        }
-
-        public string DisplayText(string lookupText, int languageNumber)// languageNumber should be changed to a global variable
-        {
-            try { return translations[headerSplit[++languageNumber]][lookupText]; }
-            catch (KeyNotFoundException)
-            {
-                if (debugdisplayedText == true)
-                    return $"Translation for {lookupText} is unknown";
-                return lookupText;
-            }
-        }
-    }
-
 
     internal class CustomTrackbar : TrackBar
     {
