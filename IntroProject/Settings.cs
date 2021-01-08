bda71cf2992092b7656fc59c8fc9cb337ccc7c61
Vincent Hoffmann <v.a.hoffmann@students.uu.nl>
2021-01-08 13:42:12 +0100

    Added general Notation

--- IntroProject/Settings.cs
+++ IntroProject/Settings.cs
@@ -3,20 +3,20 @@
     static class Settings
     {
         //geef nog alles de correcte initiele waarden, en de sliders moeten nog goed worden gescaled.
-        public static double StepSize;
-        public static int TotalEntities;
-        public static int StartEntities;
-        public static int HatchSpeed;
-        public static double SeaLevel;
-        public static int MatingCost;
-        public static int GrassGrowth = 200;
-        public static int GrassMaxFeed = 1500;
-        public static int MinTemp;
-        public static int MaxTemp;
-        public static float WalkEnergy;
-        public static float JumpEnergy;
-        public static float PassiveEnergy;
-        public static bool AddHeatMap = false;
+        public static double StepSize; //YET TO COME
+        public static int TotalEntities; //YET TO COME
+        public static int StartEntities; //YET TO COME 
+        public static int HatchSpeed; //YET TO COME
+        public static double SeaLevel; //YET TO COME
+        public static int MatingCost; //Gene & Creature
+        public static int GrassGrowth = 200; //Vegetation.cs
+        public static int GrassMaxFeed = 1500; //Vegetation.cs
+        public static int MinTemp; //YET TO COME
+        public static int MaxTemp; //YET TO COME
+        public static float WalkEnergy; //Calculator.cs
+        public static float JumpEnergy; //Calculator.cs
+        public static float PassiveEnergy; //Calculator.cs
+        public static bool AddHeatMap = false; //Hexagon.cs
         public static int LanguageIndex = 0;
     }
 }
