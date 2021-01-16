222dbc2c5cdc9970075ce3af2e53670f694ba69b
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-16 14:59:16 +0100

    Add freq used trigonometry functions Hypo and Distance

---
+++ IntroProject/Core/Math/Trigonometry.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroProject.Core.Math
+{
+    public class Point2D
+    {
+        public double X;
+        public double Y;
+    }
+
+    public static class Trigonometry
+    {
+        public static double Hypo(double a, double b) => System.Math.Sqrt(a * a + b * b);
+        public static double Hypo(Point2D a) => Hypo(a.X, a.Y);
+        public static double Distance((double, double) a, (double, double) b) => Hypo(a.Item1 - b.Item1, a.Item2 - b.Item2);
+        public static double Distance(Point2D a, Point2D b) => Distance((a.X, a.Y), (b.X, b.Y));
+    }
+}
---
+++ IntroProjectTest/Core/Math/Trigonometry.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+using IntroProject.Core.Math;
+
+namespace IntroProjectTest.Core.Math
+{
+    class TrigonometryTest
+    {
+        [TestFixture]
+        public class Hypo
+        {
+            [Test]
+            public void TestZero() =>
+                Assert.Zero(Trigonometry.Hypo(0, 0));
+
+            [Test]
+            public void TestExactTriplet() =>
+                Assert.AreEqual(5, Trigonometry.Hypo(3, 4));
+        }
+
+        [TestFixture]
+        public class Distance
+        {
+            [Test]
+            public void TestZero() =>
+                Assert.Zero(Trigonometry.Distance((42, 69), (42, 69)));
+
+            [Test]
+            public void TestExactTripletDistance() =>
+                Assert.AreEqual(5, Trigonometry.Distance((4, 2), (1, 6)));
+        }
+    }
+}
