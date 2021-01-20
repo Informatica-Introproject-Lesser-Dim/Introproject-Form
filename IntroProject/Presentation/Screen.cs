70a6378a32bf45519efe463c0989f8fa74b8df04
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-20 14:27:06 +0100

    Move StatisticsMenu to Presentation.Controls

--- IntroProject/Presentation/Screen.cs
+++ IntroProject/Presentation/Screen.cs
@@ -2,7 +2,6 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-using IntroProject.Core.Utils;
 using IntroProject.Presentation.Controls;
 
 namespace IntroProject.Presentation
@@ -61,59 +60,6 @@ namespace IntroProject.Presentation
                 mapscr.plus.Size = new Size(maxim, maxim);
             };
             mapscr.plus.Click += (object o, EventArgs ea) => { dropMenu.Show(); mapscr.plus.Hide(); };
-        }
-    }
-
-    class StatisticsMenu : UserControl
-    {
-        int r = 0;
-        public StatisticsMenu(int w, int h, EventHandler exitMenu)
-        {
-            BackColor = Color.FromArgb(123, 156, 148);
-            Size = new Size(w, h);
-            int edge = Math.Max(Size.Width, Size.Height) / 36;
-
-            Button exit = new ButtonImaged(Properties.Resources.X_icon);
-            exit.Location = new Point(2, 2);
-            exit.Size = new Size(edge, edge);
-            exit.FlatAppearance.BorderColor = Color.FromArgb(123, 156, 148);
-            exit.Click += exitMenu;
-            Controls.Add(exit);
-
-            Resize += (object o, EventArgs ea) =>
-            {
-                edge = Math.Max(Size.Width, Size.Height) / 36;
-                exit.Size = new Size(edge, edge);
-            };
-
-            Button statname1 = ButtonList("statistics");
-            Button statname2 = ButtonList("statistics");
-            Button statname3 = ButtonList("statistics");
-            Button statname4 = ButtonList("statistics");
-            Button statname5 = ButtonList("statistics");
-            Button statname6 = ButtonList("statistics");
-        }
-
-        private Button ButtonList(String name) //makes a button and next call makes a button under the previus.
-        {
-            r++;
-            int i = r, y = Math.Min(Size.Width / 30, Size.Height / 20) + 10;
-            Button button = new Button();
-            button.Text = name;
-            button.Location = new Point(0, i*y);
-            button.Size = new Size(Width / 12, Height / 20);
-            button.BackColor = Color.White;
-
-            Resize += (object o, EventArgs ea) =>
-            {
-                y = Math.Min(Size.Width / 30, Size.Height / 20) + 10;
-
-                button.Location = new Point(0, i*y);
-                button.Size = new Size(Width / 12, Height / 20);
-            };
-
-            Controls.Add(button);
-            return button;
         }
     }
 
