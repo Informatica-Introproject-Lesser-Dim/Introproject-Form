1ef62f2d9c77218f004df35f432d15f1e31f16e9
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-20 14:07:18 +0100

    Move DropMenu to Presentation.Controls

--- IntroProject/Presentation/Screen.cs
+++ IntroProject/Presentation/Screen.cs
@@ -68,65 +68,6 @@ namespace IntroProject.Presentation
         }
     }
 
-    class DropMenu : UserControl
-    {
-        public DropMenu(int w, int h, EventHandler settingsMenuOnClick, EventHandler helpMenuOnClick, EventHandler statisticsMenuOnClick, Button plus)
-        {
-            // FlatStyle to flat, and the colors of FlatAppearance MouseDownBackColor & MouseOverBackColor to transparent
-            ButtonImaged settings = new ButtonImaged(Properties.Resources.Settings_icon);
-            ButtonImaged statistics = new ButtonImaged(Properties.Resources.Graph_icon);
-            ButtonImaged help = new ButtonImaged(Properties.Resources.Help_icon);
-            ButtonImaged min = new ButtonImaged(Properties.Resources.Minus_icon);
-
-            int edge = Math.Min((int)(Size.Width * 1.2), Size.Height / 7);
-            int locY = (int)(Size.Height * .4);
-            int marge = (int)(edge * .2);
-            Size big = new Size(edge, edge);
-
-            statistics.Location = new Point(Size.Width/10, locY);
-            settings.Location = new Point(Size.Width / 10, locY + big.Width + marge);
-            help.Location = new Point(Size.Width / 10, locY + 2*big.Width + 2 *marge);
-            min.Location = new Point(5, 5);
-            min.BringToFront();
-
-            settings.Click += settingsMenuOnClick;
-            statistics.Click += statisticsMenuOnClick;
-            help.Click += helpMenuOnClick;
-            min.Click += (object o, EventArgs ea) => { Hide(); plus.Show(); };
-
-            Controls.Add(min);
-            Controls.Add(statistics);
-            Controls.Add(settings);
-            Controls.Add(help);
-
-            BackColor = Color.DimGray;
-            Size = new Size(w, h);
-
-            Resize += (object o, EventArgs ea) =>
-            {
-                edge = Math.Min((int)(Size.Width * 1.2), Size.Height / 7);
-
-                big = new Size(edge, edge);
-                statistics.Size = big;
-                settings.Size = big;
-                help.Size = big;
-                min.Size = big / 3;
-                
-                locY = (int)(Size.Height * .4);
-                marge = (int)(edge * .2);
-
-                statistics.Location = new Point(Size.Width / 10, locY);
-                settings.Location = new Point(Size.Width / 10, locY + big.Width + marge);
-                help.Location = new Point(Size.Width / 10, locY + 2 * big.Width + 2 * marge);
-            };
-        }
-    }
-    //
-    //
-    //Begin mainmenu code, het lijkt me handig als Wilfried en iemand die de omliggende code heeft gemaakt er verder naar kijken.
-    //
-    //
-    //
     class HomeMenu : UserControl
     {
         public int screenSizeX, screenSizeY, hexagonSize;
