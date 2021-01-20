6d60b6078978280c4ce44141d092963127e3d476
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-20 14:35:25 +0100

    Move CustomTrackbar to Presentation.Controls

---
+++ IntroProject/Presentation/Controls/CustomTrackBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace IntroProject.Presentation.Controls
+{
+    internal class CustomTrackbar : TrackBar
+    {
+        public CustomTrackbar(int x, int y, int min, int max)
+        {
+            Location = new Point(x, y);
+            Padding = Padding.Empty;
+            Cursor = Cursors.Hand;
+            BackColor = Color.White;
+            Minimum = min;
+            Maximum = max;
+            TickFrequency = (max - min) / 10;
+            TickStyle = TickStyle.Both;
+            SmallChange = 1;
+            LargeChange = (max - min) / 10;
+            BackColor = Color.FromArgb(123, 156, 148);
+        }
+    }
+}
--- IntroProject/Presentation/Screen.cs
+++ IntroProject/Presentation/Screen.cs
@@ -62,22 +62,4 @@ namespace IntroProject.Presentation
             mapscr.plus.Click += (object o, EventArgs ea) => { dropMenu.Show(); mapscr.plus.Hide(); };
         }
     }
-
-    internal class CustomTrackbar : TrackBar
-    {
-        public CustomTrackbar(int x, int y, int min, int max)
-        {
-            Location = new Point(x, y);
-            Padding = Padding.Empty;
-            Cursor = Cursors.Hand;
-            BackColor = Color.White;
-            Minimum = min;
-            Maximum = max;
-            TickFrequency = (max - min) / 10;
-            TickStyle = TickStyle.Both;
-            SmallChange = 1;
-            LargeChange = (max - min) / 10;
-            BackColor = Color.FromArgb(123, 156, 148);
-        }
-    }
 }
