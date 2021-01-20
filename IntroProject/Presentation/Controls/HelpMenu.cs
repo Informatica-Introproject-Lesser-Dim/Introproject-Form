e7ca23fe0d0eb190bb755010ec0caf8f1524e1e3
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-20 14:21:48 +0100

    Move HelpMenu to Presentation.Controls

---
+++ IntroProject/Presentation/Controls/HelpMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using IntroProject.Core.Utils;
+
+namespace IntroProject.Presentation.Controls
+{
+    public class HelpMenu : UserControl
+    {
+        MultipleLanguages translator = MultipleLanguages.Instance;
+
+        private Label textLabel = new Label();
+
+        public HelpMenu(int w, int h, EventHandler exitMenu)
+        {
+            this.translator = new MultipleLanguages();
+            this.BackColor = Color.FromArgb(123, 156, 148);
+            this.Size = new Size(w, h);
+
+            Button exit = new ButtonImaged(Properties.Resources.X_icon);
+            exit.Location = new Point(2, 2);
+            exit.Size = new Size(50, 50);
+            exit.FlatAppearance.BorderColor = Color.FromArgb(123, 156, 148);
+            exit.Click += exitMenu;
+
+            textLabel.Location = new Point(this.Size.Width / 2 - 200, 30);
+            textLabel.Font = new Font("Arial", 22, FontStyle.Regular);
+            textLabel.Size = new Size(400, 300);
+            textLabel.Text = translator.DisplayText("helpText");
+
+            this.Controls.Add(textLabel);
+            this.Controls.Add(exit);
+        }
+    }
+}
--- IntroProject/Presentation/Screen.cs
+++ IntroProject/Presentation/Screen.cs
@@ -64,34 +64,6 @@ namespace IntroProject.Presentation
         }
     }
 
-    class HelpMenu : UserControl
-    {
-        MultipleLanguages translator = MultipleLanguages.Instance;
-
-        private Label textLabel = new Label();
-        
-        public HelpMenu(int w, int h, EventHandler exitMenu)
-        {
-            this.translator = new MultipleLanguages();
-            this.BackColor = Color.FromArgb(123, 156, 148);
-            this.Size = new Size(w, h);
-
-            Button exit = new ButtonImaged(Properties.Resources.X_icon);
-            exit.Location = new Point(2, 2);
-            exit.Size = new Size(50, 50);
-            exit.FlatAppearance.BorderColor = Color.FromArgb(123, 156, 148);
-            exit.Click += exitMenu;
-
-            textLabel.Location = new Point(this.Size.Width / 2 - 200, 30);
-            textLabel.Font = new Font("Arial", 22, FontStyle.Regular);
-            textLabel.Size = new Size(400, 300);
-            textLabel.Text = translator.DisplayText("helpText");
-
-            this.Controls.Add(textLabel);
-            this.Controls.Add(exit);
-        }
-    }
-
     class StatisticsMenu : UserControl
     {
         int r = 0;
