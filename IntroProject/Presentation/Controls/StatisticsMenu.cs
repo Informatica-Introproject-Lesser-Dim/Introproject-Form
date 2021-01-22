613796af1d4cfa15e5ada6110d649b2d63422f57
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-22 16:33:37 +0100

    Init OxyPlot for any List as input

--- IntroProject/Presentation/Controls/StatisticsMenu.cs
+++ IntroProject/Presentation/Controls/StatisticsMenu.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
 
 namespace IntroProject.Presentation.Controls
 {
     public class StatisticsMenu : UserControl
     {
         int r = 0;
+        private PlotView plot1;
+
         public StatisticsMenu(int w, int h, EventHandler exitMenu)
         {
             BackColor = Color.FromArgb(123, 156, 148);
@@ -19,6 +26,9 @@ namespace IntroProject.Presentation.Controls
             exit.FlatAppearance.BorderColor = Color.FromArgb(123, 156, 148);
             exit.Click += exitMenu;
             Controls.Add(exit);
+
+            InitChart();
+            MakeGraph(0);
 
             Resize += (object o, EventArgs ea) =>
             {
@@ -34,9 +44,40 @@ namespace IntroProject.Presentation.Controls
             Button statname6 = ButtonList("HerbivoreSize");
         }
 
+        private void InitChart()
+        {
+            this.plot1 = new OxyPlot.WindowsForms.PlotView
+            {
+                Dock = DockStyle.Fill,
+                Location = new Point(0, 0),
+                Margin = new Padding(0),
+                Name = "plot1",
+                Size = new Size(632, 446),
+                TabIndex = 0
+            };
+            var pm = new PlotModel
+            {
+                Title = "Trigonometric functions",
+                Subtitle = "Example using the FunctionSeries",
+                PlotType = PlotType.Cartesian,
+                Background = OxyColors.White
+            };
+            pm.Series.Add(new FunctionSeries(__temp, -10, 10, 0.1, "__temp"));
+            plot1.Model = pm;
+            this.Controls.Add(this.plot1);
+        }
+
+        private double __temp(double arg)
+        {
+            var a = new List<double> { 0, 5, 3, 5, 3 };
+            int index = Math.Max(0, Math.Min(a.Count - 1, (int)arg));
+            return a[index];
+        }
+
         public void MakeGraph(int i)
         {
             //Getstats and make a graph depending on the type i given by the press of the buttons
+
         }
 
         private Button ButtonList(String name) //makes a button and next call makes a button under the previus.
