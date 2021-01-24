c550492ef2a2f295b72de6f480375a6caa3a7156
itsMacfarlane <fca.muntz@gmail.com>
2021-01-24 13:04:50 +0100

    Verander proporties vierkant achter counts

--- IntroProject/Presentation/Controls/MapScreen.cs
+++ IntroProject/Presentation/Controls/MapScreen.cs
@@ -140,7 +140,7 @@ namespace IntroProject.Presentation.Controls
                 map.TimeStep(mil);
             }
 
-            pea.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(108, 116, 150)), 0, 0, Width, Height); // Is dit wel nodig, dat het elke keer wordt gedaan?
+            pea.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(108, 116, 150)), 0, 0, Width, Height);
             if (camAutoMove)
             {
                 xCam = Width / 2 - selected.GlobalLoc.x;
@@ -163,7 +163,7 @@ namespace IntroProject.Presentation.Controls
             int[] genders = map.countMalesAndFemales();
             int[] type = map.countHerbivoresAndCarnivores();
 
-            pea.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(50, 0, 0, 0)), 50, 200, 200, 100);
+            pea.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(50, 0, 0, 0)), 50, 200, 200, 180);
             pea.Graphics.DrawString("Herbivore Count: " + type[0].ToString() +
                                     "\nCarnivore Count: " + type[1].ToString() +
                                     "\nMale Count: " + genders[0].ToString() +
