a5beb2a38bbd7fd90b9e5ac30c7d1691e8aa42bc
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2021-01-29 14:05:31 +0100

    Add DNA mascotte as resource

--- IntroProject/Properties/Resources.Designer.cs
+++ IntroProject/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace IntroProject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap DNAboi {
+            get {
+                object obj = ResourceManager.GetObject("DNAboi", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Graph_icon {
             get {
                 object obj = ResourceManager.GetObject("Graph_icon", resourceCulture);
